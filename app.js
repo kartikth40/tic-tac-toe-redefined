01bfa975f66350a8f36c677a0226e8ab75389df1
kartik <kartikth40@gmail.com>
2024-07-15 19:30:20 +0530

    added bot

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const cols = 3
 const boxes = []
 const queueOfZeroes = []
 const queueOfCrosses = []
+const vsBot = true
+const player = 0
+const bot = 1
 let gameOver = false
 const possibleWinPositions = {
   'ROW1':'row1',
@@ -45,60 +48,130 @@ function handleBoxClick(boxEle, row, col) {
   boxEle.addEventListener("click", () => {
     if(gameOver) return
     if(boxes[row] && boxes[row][col] && boxes[row][col].marked) return
+    const value = currentPlayer
+    markTheTerritory(boxEle, row, col)
+    if(value === 1) {
+      handleCross(boxEle, row, col)
+    }else {
+      handleZero(boxEle, row, col)
+    }
+    Promise.resolve().then(() => {
+      if(vsBot && value === 0 && !gameOver) runBot()
+    })
+  })
+}
+
+function markTheTerritory(boxEle, row, col, botTurn=false) {
+  let value = currentPlayer
+  if(!vsBot || value === 0 || botTurn) {
     boxEle.classList.remove('cross-hover', 'circle-hover')
     boxEle.classList.add(currentPlayer === 1 ? 'cross' : 'circle')
     boxEle.setAttribute('data-value', '2️⃣')
     let marked = true
-    let value = currentPlayer
     currentPlayer = currentPlayer === 1 ? 0 : 1
     boxes[row][col] = {element: boxEle, marked, value}
-    if(value === 1) {
-      queueOfCrosses.forEach((el, idx) => {
-        const currentValue = el.remaining
-        if(currentValue == 1){ 
-          el.element.classList.add('vanishing')
-          console.log(currentValue)
+  }
+}
 
-        }
-        const value = el.remaining === 2 ? '1️⃣' : '🔴'
-        el.element.setAttribute('data-value', value) 
-        queueOfCrosses[idx] = {...el, remaining: el.remaining-1}
-      })
-      queueOfCrosses.push({element: boxEle, row, col, remaining: 2})
-      if(queueOfCrosses.length > 3) {
-        const elementToRemove = queueOfCrosses.shift()
-        elementToRemove.element.classList.remove('cross', 'circle', 'vanishing')
-        elementToRemove.element.setAttribute('data-value', '')
-        boxes[elementToRemove.row][elementToRemove.col] = {element: elementToRemove, marked: false, value: null}
-      }
+function runBot() {
+  Promise.resolve().then(() => {
+    // Promised in order to run this only after the previous code is done running
+    const potentialWinPosition = checkIfAboutToWin(bot)
+    const potentialOpponentWinPosition = checkIfAboutToWin(player)
+    if(potentialWinPosition || potentialOpponentWinPosition) {
+      console.log(potentialWinPosition ? 'Smart Win Move -> '+potentialWinPosition : 'Smart Block -> '+ potentialOpponentWinPosition)
+      const [row,col] = potentialWinPosition || potentialOpponentWinPosition
+      const boxEle = document.querySelector(`.box-${row+1}${col+1}`)
+      markTheTerritory(boxEle, row, col, true)
+      handleCross(boxEle, row, col)
     }else {
-      queueOfZeroes.forEach((el, idx) => {
-        const currentValue = el.remaining
-        if(currentValue == 1) el.element.classList.add('vanishing')
-        const value = el.remaining === 2 ? '1️⃣' : '🔴'
-        el.element.setAttribute('data-value', value)
-        queueOfZeroes[idx] = {...el, remaining: el.remaining-1}
-      })
-      queueOfZeroes.push({element: boxEle, row, col, remaining: 2})
-      if(queueOfZeroes.length > 3) {
-        const elementToRemove = queueOfZeroes.shift()
-        elementToRemove.element.classList.remove('cross', 'circle', 'vanishing')
-        elementToRemove.element.setAttribute('data-value', '')
-        boxes[elementToRemove.row][elementToRemove.col] = {element: elementToRemove, marked: false, value: null}
+      console.log('Random Move ')
+      let randomRow = getRandomNumber()
+      let randomCol = getRandomNumber()
+    
+      while(boxes[randomRow][randomCol].marked) {
+        randomRow = getRandomNumber()
+        randomCol = getRandomNumber()
       }
-    }
-    if(won()) {
-      gameOver = true
-      const grid = document.querySelector('.grid-container')
-      grid.classList.add('mark', `${winPosition}`)
-      console.log('WON!')
+    
+      const boxEle = document.querySelector(`.box-${randomRow+1}${randomCol+1}`)
+      markTheTerritory(boxEle, randomRow, randomCol, true)
+      handleCross(boxEle, randomRow, randomCol)
     }
   })
 }
 
+function handleCross(boxEle, row, col) {
+  queueOfCrosses.forEach((el, idx) => {
+    const currentValue = el.remaining
+    if(currentValue == 1){ 
+      el.element.classList.add('vanishing')
 
-function won() {
-  return checkRows() || checkCols() || checkDiagonals()
+    }
+    const value = el.remaining === 2 ? '1️⃣' : '🔴'
+    el.element.setAttribute('data-value', value) 
+    queueOfCrosses[idx] = {...el, remaining: el.remaining-1}
+  })
+  queueOfCrosses.push({element: boxEle, row, col, remaining: 2})
+  if(queueOfCrosses.length > 3) {
+    const elementToRemove = queueOfCrosses.shift()
+    elementToRemove.element.classList.remove('cross', 'circle', 'vanishing')
+    elementToRemove.element.setAttribute('data-value', '')
+    boxes[elementToRemove.row][elementToRemove.col] = {element: elementToRemove, marked: false, value: null}
+  }
+
+  Promise.resolve().then(() => {
+    // Promised in order to run this only after the previous code is done running
+    checkIfWon()
+  })
+}
+
+function handleZero(boxEle, row, col) {
+  queueOfZeroes.forEach((el, idx) => {
+    const currentValue = el.remaining
+    if(currentValue == 1) el.element.classList.add('vanishing')
+    const value = el.remaining === 2 ? '1️⃣' : '🔴'
+    el.element.setAttribute('data-value', value)
+    queueOfZeroes[idx] = {...el, remaining: el.remaining-1}
+  })
+  queueOfZeroes.push({element: boxEle, row, col, remaining: 2})
+  if(queueOfZeroes.length > 3) {
+    const elementToRemove = queueOfZeroes.shift()
+    elementToRemove.element.classList.remove('cross', 'circle', 'vanishing')
+    elementToRemove.element.setAttribute('data-value', '')
+    boxes[elementToRemove.row][elementToRemove.col] = {element: elementToRemove, marked: false, value: null}
+  }
+
+  Promise.resolve().then(() => {
+    // Promised in order to run this only after the previous code is done running
+    checkIfWon()
+  })
+}
+
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 3)
+}
+
+function checkIfWon() {
+  const isWon = checkRows() || checkCols() || checkDiagonals()
+  if(!isWon) return
+  gameOver = true
+  const grid = document.querySelector('.grid-container')
+  grid.classList.add('mark', `${winPosition}`)
+  console.log('WON!');
+}
+
+function checkIfAboutToWin(player) {
+  let aboutToVanish = queueOfCrosses.length === 3 && queueOfCrosses[0]
+  if(player === 0) aboutToVanish = queueOfZeroes.length === 3 && queueOfZeroes[0]
+  const potentialRow = checkRowsIfAboutToWin(player, aboutToVanish)
+  const potentialCol = checkColsIfAboutToWin(player, aboutToVanish)
+  const potentialDia = checkDiagonalsIfAboutToWin(player, aboutToVanish)
+  if(potentialRow) return potentialRow
+  if(potentialCol) return potentialCol
+  if(potentialDia) return potentialDia
+  return null
 }
 
 function checkRows() {
@@ -117,6 +190,22 @@ function checkRows() {
   return false
 }
 
+function checkRowsIfAboutToWin(player, aboutToVanish) {
+  for(let row = 0; row < rows; row++) {
+    let currentRowCount = 0
+    let potentialWinPosition = null
+    for(let col = 0; col < cols; col++) {
+      if(aboutToVanish.row === row && aboutToVanish.col === col) continue
+      if(boxes[row][col].marked && boxes[row][col].value === player) currentRowCount++
+      else if(!boxes[row][col].marked) potentialWinPosition = [row,col]
+    }
+    if(currentRowCount === 2) {
+      return potentialWinPosition
+    }
+  }
+  return null
+}
+
 function checkCols() {
   for(let col = 0; col < cols; col++) {
     const currenColValue = boxes[0][col].value
@@ -130,6 +219,22 @@ function checkCols() {
       }
     }
   }
+}
+
+function checkColsIfAboutToWin(player, aboutToVanish) {
+  for(let col = 0; col < cols; col++) {
+    let currentColCount = 0
+    let potentialWinPosition = null
+    for(let row = 0; row < rows; row++) {
+      if(aboutToVanish.row === row && aboutToVanish.col === col) continue
+      if(boxes[row][col].marked && boxes[row][col].value === player) currentColCount++
+      else if(!boxes[row][col].marked) potentialWinPosition = [row,col]
+    }
+    if(currentColCount === 2) {
+      return potentialWinPosition
+    }
+  }
+  return null
 }
 
 function checkDiagonals() {
@@ -154,4 +259,33 @@ function checkDiagonals() {
     }
   }
   return false
+}
+
+function checkDiagonalsIfAboutToWin(player, aboutToVanish) {
+  let currentDiaCount = 0
+  let potentialWinPosition = null
+  for(let i = 0; i < rows; i++) {
+    if(aboutToVanish.row === i && aboutToVanish.col === i) continue
+    if(boxes[i][i].marked && boxes[i][i].value === player) currentDiaCount++
+    else if(!boxes[i][i].marked) potentialWinPosition = [i,i]
+    }
+  if(currentDiaCount === 2) {
+    return potentialWinPosition
+  }
+  currentDiaCount = 0
+  potentialWinPosition = null 
+  for(let i = 0; i < rows; i++) {
+    if(aboutToVanish.row === i && aboutToVanish.col === i) continue
+    if(boxes[i][rows-i-1].marked && boxes[i][rows-i-1].value === player) currentDiaCount++
+    else if(!boxes[i][rows-i-1].marked) potentialWinPosition = [i,rows-i-1]
+    }
+  if(currentDiaCount === 2) {
+    return potentialWinPosition
+  }
+  return null
+}
+
+
+function deepCopy(obj) {
+  return JSON.parse(JSON.stringify(obj));
 }
